9bc99b5c7a5e32fac27ff3946c3a7940c5c9240d
Billy York <byork@3cloudsolutions.com>
2020-06-17 14:22:49 -0500

    updated install script

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,6 +1,17 @@
 #Install Active Directory Components
 Add-WindowsCapability –online –Name “Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0”
 
+start-sleep 30
+
+New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
+
+invoke-webrequest -uri 'https://github.com/scautomation/AzureImageBuilder-WVD/raw/master/installFiles/MissionAppsTrugreen.zip' -outfile 'c:\temp\missioappstrugreen.zip'
+
+invoke-webrequest -uri 'https://github.com/scautomation/AzureImageBuilder-WVD/raw/master/installFiles/CSRCA01_v1.0.6_en-us.msi' -outfile 'c:\temp\CSRCA01_v1.0.6_en-us.msi'
+
+
 #Install Computer Associates
-$path="c:\temp\MissionUpdate\CSRCA01_v1.0.6_en-us.msi"
+$path="c:\temp\CSRCA01_v1.0.6_en-us.msi"
 Start-Process -FilePath msiexec.exe -ArgumentList '/i',$path,'/q' -Wait -PassThru -Verb "RunAs"
+
+Expand-Archive -Path 'c:\temp\missioappstrugreen.zip' -DestinationPath c:\
