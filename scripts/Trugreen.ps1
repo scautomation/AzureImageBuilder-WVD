3a4a25a8e402f6020b8af9f8a6096285314e6290
Billy York <byork@3cloudsolutions.com>
2020-06-18 13:49:36 -0500

    test

--- scripts/Trugreen.ps1
+++ scripts/Trugreen.ps1
@@ -5,7 +5,7 @@ start-sleep 30
 
 New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
 
-invoke-webrequest -uri 'https://github.com/scautomation/AzureImageBuilder-WVD/raw/master/installFiles/MissionAppsTrugreen.zip' -outfile 'c:\temp\missioappstrugreen.zip'
+invoke-webrequest -uri 'https://github.com/scautomation/AzureImageBuilder-WVD/raw/master/installFiles/MissionAppsTrugreen.zip' -outfile 'c:\temp\MissionAppsTrugreen.zip'
 
 invoke-webrequest -uri 'https://github.com/scautomation/AzureImageBuilder-WVD/raw/master/installFiles/CSRCA01_v1.0.6_en-us.msi' -outfile 'c:\temp\CSRCA01_v1.0.6_en-us.msi'
 
@@ -15,4 +15,4 @@ $path="c:\temp\CSRCA01_v1.0.6_en-us.msi"
 Start-Process -FilePath msiexec.exe -ArgumentList '/i',$path,'/q' -Wait -PassThru -Verb "RunAs"
 
 #expand zip file to c:\myapp
-Expand-Archive -Path 'c:\temp\MissionAppsTrugreen.zip' -DestinationPath 'c:\' -force -Confirm
+#Expand-Archive -Path 'c:\temp\MissionAppsTrugreen.zip' -DestinationPath 'c:\' -force -Confirm
